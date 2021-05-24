9ce4ee199c450e4856e2a46ea15ab34ffeb80880
Mike Harris <mike.harris@groundsure.com>
2021-05-25 00:51:06 +0100

    converted from class to functional at much cost to my metal health

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 // import weather from './lib/weather.js'
-import { makePrediction, trainModel } from './lib/model.js'
+import { makePrediction, trainModel, test } from './lib/model.js'
 
 // const s = weather.getWeatherData('2654308').then(data => console.log(data));
 const s = [8.5, 6.69,7.22,10,1011,97,3.04,285,4.18,100,0]
-const data = makePrediction(
-  s, 
-  'mike'
-)
-data
-  .then(x => console.log('Untrained: ', x))
-  .then(() => trainModel(s, 'mike', [1,0,0,0,1]))
-  .then((x) => console.log('Training: ', x))
-  .then(() => makePrediction(s, 'mike'))
-  .then(x => console.log('Testing: ', x));
 
+makePrediction(s,'mike')
+trainModel(s,'mike',[0,0,1,0,0])
